d7435eb5d425fa10f8952b87e47614ae5474fed2
janusarek <jan.usarek@gmail.com>
2018-11-22 11:21:38 +0100

    Switch other Teltonika's commands to FMB.

--- src/org/traccar/protocol/TeltonikaProtocolEncoder.java
+++ src/org/traccar/protocol/TeltonikaProtocolEncoder.java
@@ -51,15 +51,22 @@ public class TeltonikaProtocolEncoder extends BaseProtocolEncoder {
         public static final String PARAM_DISABLE = "0";
         public static final String PARAM_ENABLE = "1";
         public static final String SET_AUTHORIZED_NUMBER_1 = "1260"; // FM1000: 9.4.14
-        public static final int FMB_AUTHORIZED_NUMS_BEGIN_INDEX = 4000;
-        public static final int FMB_AUTHORIZED_NUMS_END_INDEX = 4199;
     }
     
     private static class TeltonikaFMBCommand {
+        public static final String SET_PARAM_FORMAT = "setparam %s:%s";
         public static final String POSITION_PERIODIC = "setparam 10050:%s;10054:1;10055:%s";
         public static final String POSITION_STOP = "'setparam 10000:%s;10005:120'";
         public static final String TOWING_DETECTION_ON = "setparam 11600:3;11601:1;11602:5;11603:30;11604:0;7035:1";
         public static final String TOWING_DETECTION_OFF = "setparam 11600:0";
+        public static final String SET_AUTHORIZED_NUMBER_1 = "4000";
+        public static final String GET_GPS = "getgps";
+        public static final String CPU_RESET = "cpureset";
+        public static final String GET_STATUS = "getstatus";
+        public static final String GET_INFO = "getinfo";
+        public static final String FACTORY_RESET = "defaultcfg";
+        public static final int AUTHORIZED_NUMS_BEGIN_INDEX = 4000;
+        public static final int AUTHORIZED_NUMS_END_INDEX = 4199;
     }
 
     private ChannelBuffer encodeContent(String content) {
@@ -99,7 +106,7 @@ public class TeltonikaProtocolEncoder extends BaseProtocolEncoder {
             case Command.TYPE_AUTO_ALARM_DISARM:
                 return encodeContent(TeltonikaFMBCommand.TOWING_DETECTION_OFF);
             case Command.TYPE_POSITION_SINGLE:
-                return encodeContent(TeltonikaCommand.GET_GPS);
+                return encodeContent(TeltonikaFMBCommand.GET_GPS);
             case Command.TYPE_POSITION_PERIODIC:
                 final String frequency = command.getAttributes().get(Command.KEY_FREQUENCY).toString();
                 return encodeContent(String.format(TeltonikaFMBCommand.POSITION_PERIODIC, frequency, frequency));
@@ -112,26 +119,26 @@ public class TeltonikaProtocolEncoder extends BaseProtocolEncoder {
                     centerNumber = command.getAttributes().get(Command.KEY_CENTER_NUMBER).toString();
                 }
                 return encodeContent(
-                    String.format(SET_PARAM_FORMAT, TeltonikaCommand.SET_AUTHORIZED_NUMBER_1, centerNumber)
+                    String.format(SET_PARAM_FORMAT, TeltonikaFMBCommand.SET_AUTHORIZED_NUMBER_1, centerNumber)
                 );
             case Command.TYPE_REBOOT_DEVICE:
-                return encodeContent(TeltonikaCommand.CPU_RESET);
+                return encodeContent(TeltonikaFMBCommand.CPU_RESET);
             case Command.TYPE_FACTORY_SETTINGS:
-                return encodeContent(TeltonikaCommand.FACTORY_RESET);
+                return encodeContent(TeltonikaFMBCommand.FACTORY_RESET);
             case Command.TYPE_GET_STATUS:
-                return encodeContent(TeltonikaCommand.GET_STATUS);
+                return encodeContent(TeltonikaFMBCommand.GET_STATUS);
             case Command.TYPE_GET_PARAMS:
-                return encodeContent(TeltonikaCommand.GET_INFO);
+                return encodeContent(TeltonikaFMBCommand.GET_INFO);
             case Command.TYPE_EXTENDED_CUSTOM:
                 String customCommand = command.getAttributes().get(Command.KEY_MESSAGE).toString();
                 return encodeContent(customCommand);
             case Command.TYPE_DEL_AUTHORIZED_NUMS_FIRST_100_FMB:
                 return encodeContent(
-                        getClearIdsRangeCommand(TeltonikaCommand.FMB_AUTHORIZED_NUMS_BEGIN_INDEX, 4099)
+                        getClearIdsRangeCommand(TeltonikaFMBCommand.AUTHORIZED_NUMS_BEGIN_INDEX, 4099)
                 );
             case Command.TYPE_DEL_AUTHORIZED_NUMS_SECOND_100_FMB:
                 return encodeContent(
-                        getClearIdsRangeCommand(4100, TeltonikaCommand.FMB_AUTHORIZED_NUMS_END_INDEX)
+                        getClearIdsRangeCommand(4100, TeltonikaFMBCommand.AUTHORIZED_NUMS_END_INDEX)
                 );
             default:
                 Log.warning(new UnsupportedOperationException(command.getType()));
